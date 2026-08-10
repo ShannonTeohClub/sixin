dc888f877071159ee4d25500d20005467fe7516c
ShannonTeohClub <shannon.vamtire@gmail.com>
2026-08-10 20:30:36 +0800

    restore adapter in vite config, keep paths.base in svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
-
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: false
-		}),
 		paths: {
 			base: '/sixin'
 		}
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -8,7 +9,14 @@ export default defineConfig({
 			compilerOptions: {
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			}
+			},
+			adapter: adapter({
+				pages: 'build',
+				assets: 'build',
+				fallback: '404.html',
+				precompress: false,
+				strict: false
+			})
 		})
 	]
 });
